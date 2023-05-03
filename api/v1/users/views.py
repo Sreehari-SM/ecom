e063d9b3794e6d787ea7632514a44215dff7488e
sreehari s m <sreehari.steyp@gmail.com>
2023-05-03 22:32:31 +0530

    signup form api

--- api/v1/users/views.py
+++ api/v1/users/views.py
@@ -9,6 +9,7 @@ from users.models import *
 from general.models import *
 from .serializers import *
 from api.v1.general.functions import randomnumber
+from django.utils import timezone
 
 
 @api_view(['POST'])
@@ -22,12 +23,25 @@ def signup_enter_details(request):
         country = request.data['email']
         password = request.data['email']
         if Country.objects.filter(web_code=country).exists():
+            country = Country.objects.get(web_code=country)
             if Profile.objects.filter(phone=phone, is_verified=False).exists():
+                profile = Profile.objects.get(phone=phone, is_verified=False)
                 if OtpRecord.objects.filter(phone=phone, is_applied=False).exists():
                     otp_record = OtpRecord.objects.filter(phone=phone, is_applied=False).latest("date_added")
                     if otp_record.attempts <=4:
                         otp = otp_record.otp
+                        otp.attempts += 1
+                        otp.date_updated = timezone.now()
+                        otp.save()
                         #send this otp using fast2 sms
+                        response_data = {
+                            "StatusCode": 6000,
+                            'data': {
+                                "phone" : phone,
+                                "title": "Success",
+                                "message": "successfull",
+                            }
+                        }
                     else:
                         response_data = {
                             'Statuscode' : 6001,
@@ -43,6 +57,16 @@ def signup_enter_details(request):
                         otp = otp,
                         country = country
                     )
+                    profile.otp = otp
+                    profile.save()
+                    response_data = {
+                        "StatusCode": 6000,
+                        'data': {
+                            "phone" : phone,
+                            "title": "Success",
+                            "message": "successfull",
+                        }
+                    }
             elif Profile.objects.filter(phone=phone, is_verified=True).exists():
                 response_data = {
                     'Statuscode' : 6001,
@@ -61,6 +85,62 @@ def signup_enter_details(request):
                     username = email ,
                     otp = otp             
                 )
+                if OtpRecord.objects.filter(phone=phone, is_applied=False).exists():
+                    otp_instance = OtpRecord.objects.filter(phone=phone, is_applied=False).latest('date_added')
+                    if otp_instance.attempts<=4:
+                        otp_number = otp_record.otp
+                        otp_instance.attempts += 1
+                        otp_instance.date_updated = timezone.now()
+                        otp_instance.save()
+                        response_data = {
+                            "StatusCode": 6000,
+                            'data': {
+                                "phone" : phone,
+                                "title": "Success",
+                                "message": "successfull",
+                            }
+                        }
+                    
+                    else:
+                        time_limit = otp_instance.date_updated + timezone.timedelta(days=1)
+                        if time_limit <=timezone.now():
+                            #Generate OTP
+                                otp = randomnumber(4)
+
+                                #Set OTP Record instance
+                                otp_instance = OtpRecord.objects.create(
+                                    country = country,
+                                    phone = phone,
+                                    otp = otp,                                  
+                                )
+
+                                #Set profile instance
+                                profile.otp = otp_instance.otp
+                                profile.save()
+                                response_data = {
+                                    "StatusCode": 6000,
+                                    'data': {
+                                        "phone" : phone,
+                                        "title": "Success",
+                                        "message": "successfull",
+                                    }
+                                }
+                        else:
+                            response_data = {
+                                "StatusCode": 6001,
+                                'data':{
+                                    "title": "Failed!",
+                                    "message": "You crossed the maximum limit of OTPs."
+                                }
+                            }
+                else:
+                    otp_instance = OtpRecord.objects.create(
+                        country = country,
+                        phone = phone,
+                        otp = otp,
+                    )
+                    profile.otp = otp_instance.otp
+                    profile.save()             
         else:
             response_data = {
                 'Statuscode' : 6001,
